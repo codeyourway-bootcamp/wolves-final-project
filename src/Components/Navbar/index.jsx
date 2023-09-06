c3f485ab9ba401baf0ea3dc34d5a768d117f9802
André-Felipe-Leal <andredublin2020@gmail.com>
2023-09-06 18:10:03 +0100

    criado paginas e componentes

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Navbar() {
+  return (
+    <nav>
+    <div className="nav-wrapper">
+      <a href="#" className="brand-logo">Logo</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="login.html">Login</a></li>
+        <li><a href="register.html">Register</a></li>
+      </ul>
+    </div>
+  </nav>
+  )
+}
