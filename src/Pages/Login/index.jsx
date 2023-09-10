73e297f1ca11270431e2547d0ba98e8034d4274a
André-Felipe-Leal <andredublin2020@gmail.com>
2023-09-10 19:13:52 +0100

    Terminado pagina login + Navbar

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -3,56 +3,58 @@ import { useNavigate } from "react-router-dom";
 import { user_registration } from "../../../fakedatabase";
 
 export default function Login() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-
-//   useEffect(
-//     function(){
-//         user_registration.then(
-//             function(data){
-//                 console.log(data)
-//             }
-//         )
-//     },[email, password]
-//   )
   const handleSubmit = async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    // fetch("url").then((response) => {
-    //   Response.json().then((data) => {});
-    //   if (data.length > 0){
-    //     navigate ('/dashboard');
-    //     }else{
-    //         navigate ("/login")
-    //   }
-      
-    // });
-    user_registration.then(
-    function(response){
-        const usuarioCorrespondente = response.data.find(
-            function(item){
-                console.log(item)
-                const formatDataUser = {
-                    user_email:email,
-                    user_password:password
-                }
-                const formatDataResponse = {
-                    user_email:item.user_email,
-                    user_password:item.user_password
-                }
-                return JSON.stringify(formatDataResponse) === JSON.stringify(formatDataUser)
-            }
-        )
-      
+  user_registration.then(function (response) {
+    const usuarioCorrespondente = response.data.find(function (item) {
+      const formatDataUser = {
+        user_email: email,
+        user_password: password,
+      };
+      const formatDataResponse = {
+        user_email: item.user_email,
+        user_password: item.user_password,
+      };
+      return (
+        JSON.stringify(formatDataResponse) === JSON.stringify(formatDataUser)
+      );
+    });
+    if (usuarioCorrespondente) {
+      navigate('/dashboard');
+    } else {
+      window.M.toast({html: 'Incorrect email or password.', classes: 'red darken-3', displayLength: 4000});
     }
-)
-  };
-                
+  });
+};
 
   return (
     <div className="container">
+      <h1
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        Welcome
+      </h1>
+      <p
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        Dear customer, provide your login to access the dashboard.
+      </p>
+
       <form onSubmit={handleSubmit}>
         <div className="input-field">
           <input
