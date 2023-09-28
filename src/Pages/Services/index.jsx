c170171398b77075f8242c4313056664a3482171
franciscodev <franciscorvc11@gmail.com>
2023-09-28 23:01:05 +0100

    Francisccommit

--- src/Pages/Services/index.jsx
+++ src/Pages/Services/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Button from "../../Components/Button";
 import "./styles.css";
+import { useNavigate } from "react-router-dom";
 
 function Services({ servicesList }) {
   const data = [
@@ -26,6 +27,7 @@ function Services({ servicesList }) {
     },
   ];
   const [total, setTotal] = useState([]);
+  const navigate = useNavigate();
 
   const removeElement = (DeleteTotal) => {
     const newTotal = total.filter((total) => total !== DeleteTotal);
@@ -33,55 +35,61 @@ function Services({ servicesList }) {
   };
 
   return (
-    <div className="page-services-container">
-      {data.map((item) => {
-        return (
-          <div className="services-container" key={item.serviceName}>
-            <p className="texto">{item.serviceName}</p>
-            <div
-              style={{
-                fontSize: "20px",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <p style={{ width: "40px" }}>{item.price}€</p>
-              <Button
-                id="btn"
-                isRounded={true}
-                size={"small"}
-                color={"orange"}
-                onClick={() => {
-                  setTotal([...total, item]);
+    <>
+      <h1 className="titulo">Menu:</h1>
+      <div className="page-services-container">
+        {data.map((item) => {
+          return (
+            <div className="services-container" key={item.serviceName}>
+              <p className="texto">{item.serviceName}</p>
+              <div
+                style={{
+                  fontSize: "20px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
                 }}
               >
-                select
-              </Button>
+                <p style={{ width: "40px" }}>{item.price}€</p>
+                <Button
+                  id="btn"
+                  isRounded={true}
+                  size={"small"}
+                  color={"orange"}
+                  onClick={() => {
+                    setTotal([...total, item]);
+                  }}
+                >
+                  select
+                </Button>
+              </div>
             </div>
-          </div>
-        );
-      })}
-      <div className="divisao"></div>
-      <div>
-        <h4>Itens no carrinho:</h4>
-        {total.map(function (item) {
-          return (
-            <h5 className="conteiner">
-              {item.serviceName} - <span className="total">{item.price}€</span>{" "}
-              <button
-                onClick={() => removeElement(item)}
-                className="bi bi-trash-fill"
-                id="close"
-              ></button>
-            </h5>
           );
         })}
-        <div className="divisao2"></div>
+        <div className="divisao"></div>
+        <div>
+          <h4>Itens no carrinho:</h4>
+          {total.map(function (item) {
+            return (
+              <h5 className="conteiner">
+                {item.serviceName} -{" "}
+                <span className="total">{item.price}€</span>{" "}
+                <button
+                  onClick={() => removeElement(item)}
+                  className="bi bi-trash-fill"
+                  id="close"
+                ></button>
+              </h5>
+            );
+          })}
+          <div className="divisao2"></div>
 
-        <button id="checkout">Checkout</button>
+          <button id="checkout" onClick={() => navigate("/checkout")}>
+            Checkout
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
