24e6f8adb788106c0a2721c0aa30e7141ecf466f
estebanlarrotech <esteban.larrotech@gmail.com>
2023-09-28 01:27:59 +0100

    dashboard template

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -1,7 +1,17 @@
-
+import './style.css' 
 
 export default function Dashboard() {
   return (
-    <><h1>ola</h1></>
+    <>
+    <div className="banner">
+  <div>
+    <h1>Welcome to QuickBook</h1>
+    <h5>Sign up for free and get high quality customers</h5>
+  </div>  
+</div>
+   <div className="butnotme">
+   <button>Get started</button>
+   </div>
+    </>
   )
 }
